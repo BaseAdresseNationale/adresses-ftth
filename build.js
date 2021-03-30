11e43ab4655b5825e01c277ef183e74324693b41
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2021-03-30 17:57:31 +0200

    Revert "Allow use up to 12GB memory"
    This reverts commit 78f3ded9fb5029b2b7e0fb6a80e3818e377897b3.

--- build.js
+++ build.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node --max-old-space-size=12288
+#!/usr/bin/env node --max-old-space-size=8192
 const {join} = require('path')
 const {createReadStream} = require('fs')
 const {Transform} = require('stream')
